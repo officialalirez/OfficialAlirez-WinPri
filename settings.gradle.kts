387f98ede0174abba7bc14f72193c9c176f4ac05
PvAlirez <64715752+alirezahms@users.noreply.github.com>
2026-07-01 12:01:43 +0000

    Updated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,21 +19,6 @@ dependencyResolutionManagement {
         maven { url = "https://jitpack.io" }
     }
 }
-    }
-    mavenCentral()
-    gradlePluginPortal()
-  }
-}
-
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
-
-dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
-}
 
 rootProject.name = "My Application"
 
