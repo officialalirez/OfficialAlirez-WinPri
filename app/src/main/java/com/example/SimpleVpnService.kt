1a0564e28fc479ba99a3de86275ef5db658907e6
PvAlirez <64715752+alirezahms@users.noreply.github.com>
2026-07-01 11:57:07 +0000

    Updated

--- app/src/main/java/com/example/SimpleVpnService.kt
+++ app/src/main/java/com/example/SimpleVpnService.kt
@@ -57,7 +57,7 @@ class SimpleVpnService : VpnService() {
 
             val builder = Builder()
                 .setSession("Win2ray Shield")
-                .setMtu(VPN_MTU)
+                .setMTU(VPN_MTU)
                 .addAddress(VPN_ADDRESS, 255)
                 .addRoute(VPN_ROUTE, 0)
                 .addDnsServer("1.1.1.1")
